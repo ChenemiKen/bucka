010ade4a96e22fce60854d4e8a43f0aebd4c9caf
ChenemiKen <chenemiken15@gmail.com>
2022-12-06 09:31:48 +0100

    initial commit

---
+++ src/main/java/com/chenemiken/bucka/BuckaApplication.java
@@ -0,0 +1,13 @@
+package com.chenemiken.bucka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chenemiken/bucka/BuckaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenemiken.bucka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
